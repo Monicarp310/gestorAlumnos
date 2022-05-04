8374bad8c806321023d957e033f9cdf6c83461f1
Mónica Rodríguez Pérez <monicarodriguez@devanddel.com>
2022-05-04 10:53:27 +0200

    fix error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule, MatInputModule } from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
 import { AltaAlumnoComponent } from './altaAlumno/components/altaAlumno/components/alta-alumno.component';
@@ -26,11 +26,9 @@ import { EncryptionService } from './utils/services/encryption.service';
     MatSelectModule,
     MatDialogModule,
     MatFormFieldModule,
-    MatInputModule
   ],
   exports: [
-    MatInputModule
-  ]
+  ],
   providers: [EncryptionService],
   bootstrap: [AppComponent]
 })
